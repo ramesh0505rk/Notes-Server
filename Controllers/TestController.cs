7859b98b6cede28a7a82d00ceaccd0e0e7fc1e51
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-02-28 10:43:47 +0530

    Implemented first level access token validation.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet("hi")]
+        public IActionResult Get()
+        {
+            return Ok("Hi user");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,18 +1,19 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://dev-42709845.okta.com/oauth2/default";
+        options.Audience = "api://default";
+    });
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
