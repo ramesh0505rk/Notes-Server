513409cf46c027453c0ed4fc8145b8b115960710
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-03-02 19:23:11 +0530

    Implemented models.

---
+++ Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace NotesServer.Models
+{
+    public class Note
+    {
+        public string NoteId { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
