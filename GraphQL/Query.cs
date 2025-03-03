18c72c8d85a1f6638e8c7ca0b5e9852b6bc83faa
RAMESHKUMAR <t476797@deluxe.com>
2025-03-03 14:27:08 +0530

    Initialized query - test, added dbcontext singleton

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -1,6 +1,22 @@
-﻿namespace NotesServer.GraphQL
+﻿using GraphQL.Types;
+using NotesServer.Context;
+using NotesServer.GraphQL.Types;
+using NotesServer.Models;
+
+namespace NotesServer.GraphQL
 {
-    public class Query
+    public class Query:ObjectGraphType
     {
+        public Query(DbContext dbContext) {
+            //Field<ListGraphType<NoteType>>(
+            //    "notes",
+            //    resolve: context =>
+            //    {
+            //        var query = "select * from Notes";
+            //        using var connection = dbContext.CreateConnection();
+            //        return connection.Query<Note>(query).ToList();
+            //    }
+            //    );
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using NotesServer.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.Authority = "https://dev-42709845.okta.com/oauth2/default";
         options.Audience = "api://default";
     });
+
+builder.Services.AddSingleton<DbContext>();
 
 var app = builder.Build();
 
