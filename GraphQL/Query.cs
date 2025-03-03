275d939be662e4fe9d9e73665b67f67a5e1239ed
RAMESHKUMAR K <ramesh0505rk@gmail.com>
2025-03-03 15:24:23 +0530

    Field update according to new version.

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -1,22 +1,27 @@
-﻿using GraphQL.Types;
+﻿using Dapper;
+using GraphQL.Resolvers;
+using GraphQL.Types;
 using NotesServer.Context;
 using NotesServer.GraphQL.Types;
 using NotesServer.Models;
 
 namespace NotesServer.GraphQL
 {
-    public class Query:ObjectGraphType
+    public class Query : ObjectGraphType
     {
-        public Query(DbContext dbContext) {
-            //Field<ListGraphType<NoteType>>(
-            //    "notes",
-            //    resolve: context =>
-            //    {
-            //        var query = "select * from Notes";
-            //        using var connection = dbContext.CreateConnection();
-            //        return connection.Query<Note>(query).ToList();
-            //    }
-            //    );
+        public Query(DbContext dbContext)
+        {
+            AddField(new FieldType
+            {
+                Name = "notes",
+                Type = typeof(ListGraphType<NoteType>),
+                Resolver = new FuncFieldResolver<object>(context =>
+                {
+                    var query = "SELECT * FROM Notes";
+                    using var connection = dbContext.CreateConnection();
+                    return connection.Query<Note>(query).ToList();
+                })
+            });
         }
     }
 }
