9bc9624e09a462d60c005af3b07bc5ec4a1375d1
RAMESHKUMAR <t476797@deluxe.com>
2025-03-03 20:10:17 +0530

    AddNote - Initial

--- GraphQL/Mutation.cs
+++ GraphQL/Mutation.cs
@@ -14,5 +14,15 @@ namespace NotesServer.GraphQL
             await connection.ExecuteAsync(query, parameters);
             return new User { UserId = UserId, UserName = UserName, UserEmail = UserEmail, };
         }
+        //public async Task<Note> AddNote(string UserId, string Title, string Content, [Service] DbContext context)
+        //{
+        //    //Guid NoteId = Guid.NewGuid();
+        //    //var query = "insert into Notes (NoteId,UserId,Title,Content) values (@NoteId,@UserId,@Title,@Content)";
+        //    //using var connection = context.CreateConnection();
+        //    //var parameters = new {NoteId=NoteId,UserId=UserId,Title=Title,Content=Content};
+        //    //await connection.ExecuteAsync(query, parameters);
+        //    //var createdDateTime = connection.QueryAsync("select ")
+        //    //return new Note { NoteId=NoteId,UserId=UserId,Title=Title,Content=Content,CreatedDate };
+        //}
     }
 }
