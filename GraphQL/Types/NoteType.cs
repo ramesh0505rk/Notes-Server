fc7167551dc5e84cf952a0a0e104f4f251ccbada
RAMESHKUMAR <t476797@deluxe.com>
2025-03-03 14:15:31 +0530

    Initialized user and note type.

--- GraphQL/Types/NoteType.cs
+++ GraphQL/Types/NoteType.cs
@@ -1,6 +1,17 @@
-﻿namespace NotesServer.GraphQL.Types
+﻿using GraphQL.Types;
+using NotesServer.Models;
+
+namespace NotesServer.GraphQL.Types
 {
-    public class NoteType
+    public class NoteType : ObjectGraphType<Note>
     {
+        public NoteType()
+        {
+            Field(x => x.NoteId, type: typeof(IdGraphType)).Description("Id of the note");
+            Field(x => x.UserId, type: typeof(IdGraphType)).Description("Id of the user");
+            Field(x => x.Title).Description("Title of the note");
+            Field(x => x.Content).Description("Content of the note");
+            Field(x => x.CreatedDate, type: typeof(DateTimeGraphType)).Description("Timestamp when note was created");
+        }
     }
 }
--- GraphQL/Types/UserType.cs
+++ GraphQL/Types/UserType.cs
@@ -1,6 +1,15 @@
-﻿namespace NotesServer.GraphQL.Types
+﻿using GraphQL.Types;
+using NotesServer.Models;
+
+namespace NotesServer.GraphQL.Types
 {
-    public class UserType
+    public class UserType : ObjectGraphType<User>
     {
+        public UserType()
+        {
+            Field(x => x.UserId, type: typeof(IdGraphType)).Description("Id of the user");
+            Field(x => x.UserName).Description("Name of the user");
+            Field(x => x.UserEmail).Description("Email of the user");
+        }
     }
 }
